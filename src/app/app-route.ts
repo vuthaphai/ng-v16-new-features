48f2d69cb0c4077752ec215193e915f26562a8ae
vuthaphai <vuthaphai@gmail.com>
2024-01-28 10:31:59 +0700

    finished: converted to standalone component

---
+++ src/app/app-route.ts
@@ -0,0 +1,8 @@
+import { Routes } from "@angular/router";
+
+
+export const APP_ROUTE: Routes = [
+
+];
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { APP_ROUTE } from './app/app-route';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [provideRouter(APP_ROUTE)]
+})
   .catch(err => console.error(err));
