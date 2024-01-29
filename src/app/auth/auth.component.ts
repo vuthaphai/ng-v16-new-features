260ee33fd8db0b407feee8ec060088a390f53593
Vutha Phai (ផៃ វុត្ថា) <vutha_phai@manulife.com>
2024-01-29 22:31:06 +0700

    add auth

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../shared/component/header/header.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-auth',
-    standalone: true,
-    templateUrl: './auth.component.html',
-    styleUrls: ['./auth.component.scss'],
-    imports: [CommonModule, HeaderComponent]
+  selector: 'app-auth',
+  standalone: true,
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
+  imports: [CommonModule, RouterModule, HeaderComponent],
 })
-export class AuthComponent {
-
-}
+export class AuthComponent {}
