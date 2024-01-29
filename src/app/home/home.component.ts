4f810f358e1ce0c5c6acd3fb2393667e03991b19
Vutha Phai (ផៃ វុត្ថា) <vutha_phai@manulife.com>
2024-01-29 23:24:50 +0700

    basic signal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from "../shared/component/header/header.component";
+import { HeaderComponent } from '../shared/component/header/header.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    imports: [CommonModule,RouterModule, HeaderComponent]
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  imports: [CommonModule, RouterModule, HeaderComponent],
 })
 export class HomeComponent {
-
+  title = signal('');
+  changeTitle(event: Event) {
+    const title = (event.target as HTMLInputElement).value;
+    this.title.set(title);
+  }
 }
