04c7e7b26b071c1a5a67b250ec9d6c906d644c9f
Vutha Phai (ផៃ វុត្ថា) <vutha_phai@manulife.com>
2024-01-30 19:15:50 +0700

    wip

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from "../shared/component/header/header.component";
+import { HeaderComponent } from '../shared/component/header/header.component';
 import { RouterModule } from '@angular/router';
 
-@Component({
-    selector: 'app-home',
-    standalone: true,
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    imports: [CommonModule,RouterModule, HeaderComponent]
-})
-export class HomeComponent {
+interface UserInterface {
+  id: string;
+  name: string;
+}
 
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  imports: [CommonModule, RouterModule, HeaderComponent],
+})
+export class HomeComponent implements OnInit {
+  title = signal('');
+
+  users = signal<UserInterface[]>([]);
+  // title: string = '';
+
+  changeTitle(event: Event) {
+    const title = (event.target as HTMLInputElement).value;
+    this.title.set(title);
+    // this.title = title;
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.users.set([{ id: '1', name: 'Foo' }]);
+      console.log(this.users());
+    }, 2000);
+  }
 }
