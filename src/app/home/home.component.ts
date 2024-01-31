c9c529d118f5a7ab3ea05a96be7a37ccef9bd1a7
Vutha Phai (ផៃ វុត្ថា) <vutha_phai@manulife.com>
2024-01-31 19:17:52 +0700

    finished: practice signals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, computed, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../shared/component/header/header.component';
 import { RouterModule } from '@angular/router';
@@ -17,9 +17,13 @@ interface UserInterface {
 })
 export class HomeComponent implements OnInit {
   title = signal('');
-
   users = signal<UserInterface[]>([]);
-  // title: string = '';
+  // title: string = ''; //normal variable
+  titleChangeEffect = effect(() => {
+    console.log('titleChangeEffect', this.title());
+  });
+
+  totalUsers = computed(() => this.users().length);
 
   changeTitle(event: Event) {
     const title = (event.target as HTMLInputElement).value;
@@ -29,7 +33,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.users.set([{ id: '1', name: 'Foo' }]);
+      // this.users.set([{ id: '1', name: 'Foo' }]); // first way to assing values to users
+      // this.users.update((prevUsers) => [
+      //   ...prevUsers,
+      //   { id: '1', name: 'Foo' },
+      // ]); // second way to assing values to users
+
+      this.users.mutate((currentUsers) =>
+        currentUsers.push({ id: '1', name: 'Foo' })
+      );
+
       console.log(this.users());
     }, 2000);
   }
