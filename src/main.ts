504d60f1e03af645f7456b98f5a5366c92f14de4
Vutha Phai (ផៃ វុត្ថា) <vutha_phai@manulife.com>
2024-02-03 13:29:11 +0700

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
