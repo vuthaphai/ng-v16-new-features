8b033073970666aeddafeae1b2f40af08ad8e9d4
Vutha Phai (ផៃ វុត្ថា) <vutha_phai@manulife.com>
2024-02-03 18:28:45 +0700

    fix directive

--- src/app/welcome/welcome.module.ts
+++ src/app/welcome/welcome.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { DetailsComponent } from './details/details.component';
 import { WelcomeComponent } from './welcome.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [WelcomeComponent, DetailsComponent],
-  imports: [CommonModule, WelcomeRoutingModule],
+  imports: [CommonModule, WelcomeRoutingModule, SharedModule],
 })
 export class WelcomeModule {}
